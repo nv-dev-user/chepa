1aa3568dc5e659bbe256eb85b18cbba8eea20826
nv-dev <nv-dev@outlook.fr>
2026-04-08 16:50:09 +0200

    fixed npc

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,44 @@
-fn initialize() {
-    println!("Initializing game...");
-    // Here you can add code to load resources, set up the game world, etc.
+use chepa::models::armor::Armor;
+use chepa::models::weapon::Weapon;
+use chepa::models::zone::Zone;
+use chepa::models::npc::NPC;
+use chepa::models::player::Player;
+
+struct Game {
+    weapons: Vec<Weapon>,
+    zones: Vec<Zone>,
+    armors: Vec<Armor>,
+    npcs: Vec<NPC>,
+    players: Vec<Player>,
 }
 
-fn update() {
-    println!("Updating game state...");
-    // Here you can add code to update the game logic, handle user input, etc.
-}
+impl Game {
+    pub fn new() -> Self {
+        Game {
+            weapons: Vec::new(),
+            zones: Vec::new(),
+            armors: Vec::new(),
+            npcs: Vec::new(),
+            players: Vec::new(),
+        }
+    }
 
-fn render() {
-    println!("Rendering game...");
-    // Here you can add code to draw the game world, UI, etc.
+    pub fn update(&mut self) {
+        println!("Updating game state...");
+        // Here you can add code to update the game logic, handle user input, etc.
+    }
+
+    pub fn render(&self) {
+        println!("Rendering game...");
+        // Here you can add code to draw the game world, UI, etc.
+    }
 }
 
 fn main() {
-    initialize();
+    let mut game = Game::new();
 
     loop {
-        update();
-        render();
+        game.update();
+        game.render();
     }
 }
